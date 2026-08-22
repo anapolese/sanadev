f78bcb17ee74cb3bb59c1ea8425d68b976acd0be
Ana Polese <anapolese2@gmail.com>
2026-08-22 13:02:25 -0300

    [SANA-0004] Resolve vulnerabilities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Historia from './pages/historia';
 import Home from './pages/home';
+import LoaderWidget from './components/LoaderWidget';
 import WhatsAppLink from './components/WhatsAppLink';
+import { isLoading, onDomContentLoaded } from './utils/utils';
 import './App.css';
 
 function App() {
+  const [isDomLoading, setIsDomLoading] = useState(() => isLoading());
+
+  useEffect(() => {
+    if (!isDomLoading) return undefined;
+
+    return onDomContentLoaded(() => setIsDomLoading(false));
+  }, [isDomLoading]);
+
+  if (isDomLoading) {
+    return <LoaderWidget />;
+  }
+
   return (
     <div className="Sana">
       <Header />
---
+++ src/components/LoaderWidget/index.js
@@ -0,0 +1,7 @@
+function LoaderWidget() {
+	return (
+		<div className="LoaderWidget" role="status" aria-label="Loading" />
+	);
+}
+
+export default LoaderWidget;
